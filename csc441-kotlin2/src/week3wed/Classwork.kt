7fa9d5dfdd80eee1867d6a41476df7539deeca4c
Grace Scaglione <scaglige@lemoyne.edu>
2026-09-09 18:15:20 -0400

    adding week 3 classwork

---
+++ csc441-kotlin2/src/week3wed/Classwork.kt
@@ -0,0 +1,40 @@
+package week3wed
+
+fun main() {
+    println("---Step 1 - main and output---")
+    println("CSC 441")
+    println("Kotlin, first day")
+
+    println("---Step 2 - val and var---")
+    val course = "CSC 441"
+    var weekNumber = 3
+    weekNumber += 1
+
+    println(course)
+    println(weekNumber)
+
+    println("---Step 3 - the four basic type---")
+    val name = "Grace"
+    val age = 20
+    val gpa = 3.3
+    val learning = true
+
+    println(name)
+    println(age)
+    println(gpa)
+    println(learning)
+
+    println("---Step 4 - String templates")
+    println("$name learns $course")
+    //println(name + " " + "teaches" + " " + course)
+    //println(name + " teaches " + course)
+    println("Next tear: ${age + 1}")
+    println("name length: ${name.length}")
+
+    println("---Step 5: Reading input---")
+    print("What's your name? ")
+    val yourName = readlnorNull()
+    println("Hello, $yourName")
+
+
+}

a829f406eef55249bdcce1f3d672d73f0790e657
Grace Scaglione <scaglige@lemoyne.edu>
2026-09-18 14:57:02 -0400

    wed lab 4

--- csc441-kotlin/src/week4wed/Lab.kt
+++ csc441-kotlin/src/week4wed/Lab.kt
@@ -1,41 +1,57 @@
 package week4wed
 
 fun main() {
-    println("---Requirement 1: takes nothing, returns nothing---")
+    println("--- Requirement 1: takes nothing, returns nothing ---")
     describeToday()
 
-    println("---Requirement 2: takes nothing, returns a string---")
-    favoriteThing()
+    println("--- Requirement 2: takes nothing, returns a string ---")
+    println(favoriteThing())
 
-    println("---Requirement 3: takes one parameter---")
-    pickOne(2)
+    println("--- Requirement 3: takes one parameter ---")
+    println(pickOne(2))
 
-    println("---Requirement 4: short---")
+    println("--- Requirement 4: short ---")
+    println(pickOneShort(2))
 
-    println("---Requirement 5: takes two parameters---")
+    println("--- Requirement 5: takes two parameters ---")
+    println(pickWithDefault())
+    println(pickWithDefault(name = "Grace"))
 }
 
 fun describeToday() {
-    println("Middle of the week")
+    println("Today is the middle of the week")
 }
 
 fun favoriteThing(): String {
-    return "the night sky"
+    return "My favorite thing is music"
 }
 
 fun pickOne(number: Int): String {
     return when (number) {
-        1 -> "first choice"
-        2 -> "second choice"
-        else -> "different choice"
+        1 -> "Spring"
+        2 -> "Summer"
+        3 -> "Autumn"
+        4 -> "Winter"
+        else -> "Not a season"
     }
 
 }
 
-fun pickOneShort(number: Int): String {
-
+fun pickOneShort(number: Int): String = when (number){
+    1 -> "Spring"
+    2 -> "Summer"
+    3 -> "Autumn"
+    4 -> "Winter"
+    else -> "Not a season"
 }
 
-fun pickWithDefault(number: Int = 1, name: String = "you") {
-
+fun pickWithDefault(number: Int = 1, name: String = "you"): String {
+    val season = when (number) {
+        1 -> "Spring"
+        2 -> "Summer"
+        3 -> "Autumn"
+        4 -> "Winter"
+        else -> "Not a season"
+    }
+    return "$name: $season"
 }
